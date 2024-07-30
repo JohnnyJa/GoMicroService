cc668ee2ccabf739a967688a4a4a544a58be3fbd
Іван Боровков <88048424+JohnnyJa@users.noreply.github.com>
2024-07-30 18:42:40 +0300

    Basic auth realised

---
+++ authentification-service/cmd/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"authentification-service/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting authentification service on port", webPort)
+
+	// TODO connect to DB
+
+	//set up config
+
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panicf("server failed to start: %v", err)
+	}
+}
