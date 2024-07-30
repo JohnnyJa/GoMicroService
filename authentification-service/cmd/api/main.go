d503041e3396c58afd856d617086ab9c7fd151b3
Іван Боровков <88048424+JohnnyJa@users.noreply.github.com>
2024-07-30 18:52:38 +0300

    Connection to DB added

--- authentification-service/cmd/api/main.go
+++ authentification-service/cmd/api/main.go
@@ -6,9 +6,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPort = "80"
+
+var counts int64
 
 type Config struct {
 	DB     *sql.DB
@@ -19,10 +27,17 @@ func main() {
 	log.Println("Starting authentification service on port", webPort)
 
 	// TODO connect to DB
+	conn, err := connectToDB()
+	if err != nil {
+		log.Panicf("Could not connect to DB: %v", err)
+	}
 
 	//set up config
 
-	app := Config{}
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -31,5 +46,39 @@ func main() {
 
 	if err := srv.ListenAndServe(); err != nil {
 		log.Panicf("server failed to start: %v", err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() (*sql.DB, error) {
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Could not connect to DB. Retrying in 5 seconds")
+			counts++
+		} else {
+			log.Println("Connected to DB")
+			return connection, nil
+		}
+
+		if counts >= 5 {
+			return nil, fmt.Errorf("Connection lost after %d retries", counts)
+		}
+
+		log.Println("Retrying in 2 seconds")
+		time.Sleep(2 * time.Second)
 	}
 }
