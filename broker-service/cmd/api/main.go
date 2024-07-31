6fd237c5843e703fcc4d5ec73d7b58522b091819
Іван Боровков <88048424+JohnnyJa@users.noreply.github.com>
2024-07-31 21:03:58 +0300

    added handler to broker

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +12,41 @@ import (
 const webPort = "80"
 
 type Config struct{}
+
+func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
+	jsonData, _ := json.MarshalIndent(msg, "", "\t")
+
+	mailServiceURL := "http://mail-service/senf"
+
+	request, err := http.NewRequest("POST", mailServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error sending mail"))
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Mail sent to " + msg.To,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
 
 func main() {
 	app := Config{}
